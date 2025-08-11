3310feaaed9f78692138190cfc4206638649b26c
username_github <email_github>
2025-08-11 10:40:35 +0700

    "vlab"

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3a86ff",
+        secondary: "#8338ec",
+        "custom-blue": {
+          100: "#dbeafe",
+          500: "#3b82f6",
+          900: "#1e3a8a",
+        },
+      },
+    },
+  },
+};
