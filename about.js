8a42d7f6a833348963b2bbd16ec79282292aaeb2
ADZ-003 <muhammadadzkiya821@gmail.com>
2025-08-25 09:12:28 +0700

    main

---
+++ about.js
@@ -0,0 +1,14 @@
+{/* <script>
+      const menuBtn = document.getElementById("menu-btn");
+      const mobileMenu = document.getElementById("mobile-menu");
+
+      menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+      });
+
+      document.querySelectorAll("#mobile-menu a").forEach((link) => {
+        link.addEventListener("click", () => {
+          mobileMenu.classList.add("hidden");
+        });
+      });
+    </script> */}
