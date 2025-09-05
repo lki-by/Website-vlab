6b1b45cc8884075e2fa9d54bfbf9a5a893122f6e
username_github <email_github>
2025-09-05 15:06:03 +0700

    "Update-vlab"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 module.exports = {
   theme: {
     extend: {
-      colors: {
-        primary: "#3a86ff",
-        secondary: "#8338ec",
-        "custom-blue": {
-          100: "#dbeafe",
-          500: "#3b82f6",
-          900: "#1e3a8a",
-        },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
+  plugins: [],
 };
